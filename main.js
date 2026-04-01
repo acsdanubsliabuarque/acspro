ad32ef86c2b2cf58e5e5a787e43b69f0151f7029
acsdanubsliabuarque <acsdaniloubsliaburque@gmail.com>
2026-03-31 22:56:02 -0300

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', async () => {
-    CustomModals.init();
-    Nav.init();
-    await DB.init();
-    console.log("ACS PRO v12.0 - Inicializado com sucesso.");
-});
